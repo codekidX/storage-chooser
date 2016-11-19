a3d87fc0b659ba164d163b318c3da29ee3900334
codekidX <ckrackerx15@gmail.com>
2016-11-19 19:59:15 +0530

    finally using DiskUtil class yay :tada:

--- src/main/java/com/codekidlabs/storagechooser/utils/DiskUtil.java
+++ src/main/java/com/codekidlabs/storagechooser/utils/DiskUtil.java
@@ -1,5 +1,11 @@
 package com.codekidlabs.storagechooser.utils;
 
 
+import android.os.Build;
+
 public class DiskUtil {
+
+    public static int getSdkVersion() {
+        return Build.VERSION.SDK_INT;
+    }
 }
