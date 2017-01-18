012b159e7a8e824180e0b532e0c891c64972f19f
codekidX <ckrackerx15@gmail.com>
2017-01-18 16:56:10 +0530

    remove unwanted lines in demo

--- app/src/main/java/com/codekidlabs/storagechooserdemo/MainActivity.java
+++ app/src/main/java/com/codekidlabs/storagechooserdemo/MainActivity.java
@@ -92,10 +92,6 @@ public class MainActivity extends AppCompatActivity {
                 });
 
                 StorageChooserView.setViewSc(StorageChooserView.SC_LAYOUT_SHEET);
-                int[] nightModerColors = {R.color.memory_status_color, R.color.memory_bar_color,
-                        R.color.new_folder_color, R.color.select_color, R.color.cancel_color};
-                StorageChooserView.setNightColors(nightModerColors);
-                storageChooser.setMode(StorageChooser.NIGHT_MODE);
                 storageChooser.show();
             }
         });
