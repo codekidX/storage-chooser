e78ee43492617217ad03cea221ea870f21a9c4fa
codekidX <ckrackerx15@gmail.com>
2017-01-18 17:42:39 +0530

    update to simple demo

--- app/src/main/java/com/codekidlabs/storagechooserdemo/MainActivity.java
+++ app/src/main/java/com/codekidlabs/storagechooserdemo/MainActivity.java
@@ -3,10 +3,14 @@ package com.codekidlabs.storagechooserdemo;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -22,6 +26,7 @@ import com.codekidlabs.storagechooserdemo.utils.TypefaceUtil;
 public class MainActivity extends AppCompatActivity {
 
     Context context;
+    private static FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,74 +38,71 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        TextView scTitle = (TextView) findViewById(R.id.sc_title);
-        TypefaceUtil.setTypefaceBold(context, scTitle);
+        fragmentManager = getSupportFragmentManager();
 
-        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                BottomSheetDialogFragment reportSheet = new ReportBottomSheet();
-                reportSheet.show(getSupportFragmentManager(), "report_sheet");
-            }
-        });
+        getFragmentManager().beginTransaction().replace(R.id.container, new DemoPreference()).commit();
 
-        Button storageChooserButton = (Button) findViewById(R.id.storage_chooser_button);
-        TypefaceUtil.setTypefaceBold(context,storageChooserButton);
-        storageChooserButton.setBackgroundColor(getColorFromResource(context,android.R.color.white));
-
-        storageChooserButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                StorageChooser storageChooser = new StorageChooser.Builder()
-                        .withActivity(MainActivity.this)
-                        .withFragmentManager(getSupportFragmentManager())
-                        .withPredefinedPath("/Downloads/OpenGApps")
-                        .withPreference(sharedPreferences)
-                        .build();
-
-                storageChooser.show();
-            }
-        });
-
-        Button storageChooserButtonMemorybar = (Button) findViewById(R.id.storage_chooser_button_memorybar);
-        TypefaceUtil.setTypefaceBold(context,storageChooserButtonMemorybar);
-        storageChooserButtonMemorybar.setBackgroundColor(getColorFromResource(context,android.R.color.white));
-
-        storageChooserButtonMemorybar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                StorageChooser storageChooser = new StorageChooser.Builder()
-                        .withActivity(MainActivity.this)
-                        .withFragmentManager(getSupportFragmentManager())
-                        .withMemoryBar(true)
-                        .setDialogTitle("Ashish's Chooser")
-                        .setInternalStorageText("My Internal")
-                        .allowCustomPath(true)
-                        .allowAddFolder(true)
-                        .withPredefinedPath("/Downloads/OpenGApps")
-                        .withPreference(sharedPreferences)
-                        .build();
-
-                storageChooser.setOnSelectListener(new StorageChooser.OnSelectListener() {
-                    @Override
-                    public void onSelect(String path) {
-                        Log.e("SELECTLISTENER_PATH", path);
-                    }
-                });
-
-                StorageChooserView.setViewSc(StorageChooserView.SC_LAYOUT_SHEET);
-                storageChooser.show();
-            }
-        });
-
-        TextView explanationText = (TextView) findViewById(R.id.explanation_text);
-        TypefaceUtil.setTypefaceRegular(context, explanationText);
     }
 
     public static int getColorFromResource(Context context, int id) {
         return ContextCompat.getColor(context, id);
+    }
+
+    public static class DemoPreference extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_demo);
+
+            PreferenceScreen preferenceScreen = getPreferenceScreen();
+            Preference overviewPref = preferenceScreen.findPreference("pref_overview");
+            Preference customPref = preferenceScreen.findPreference("pref_overview");
+            Preference filePickerPref = preferenceScreen.findPreference("pref_overview");
+
+            overviewPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    StorageChooser chooser = new StorageChooser.Builder()
+                            .withActivity(getActivity())
+                            .withFragmentManager(fragmentManager)
+                            .withMemoryBar(true)
+                            .build();
+
+                    chooser.show();
+                    return true;
+                }
+            });
+
+            customPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    StorageChooser chooser = new StorageChooser.Builder()
+                            .withActivity(getActivity())
+                            .withFragmentManager(fragmentManager)
+                            .withMemoryBar(true)
+                            .build();
+
+                    chooser.show();
+                    return true;
+                }
+            });
+
+
+            filePickerPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    StorageChooser chooser = new StorageChooser.Builder()
+                            .withActivity(getActivity())
+                            .withFragmentManager(fragmentManager)
+                            .withMemoryBar(true)
+                            .build();
+
+                    chooser.show();
+                    return true;
+                }
+            });
+        }
     }
 }
