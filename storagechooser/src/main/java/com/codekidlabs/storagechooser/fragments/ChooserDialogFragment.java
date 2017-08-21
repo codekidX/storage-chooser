7ee5fa3dc75b051cef9d091bded4b02ee04ee50d
Takuma <vmgsahm1@gmail.com>
2017-08-21 12:50:16 +0800

    Fix if no sdcard path will be crashed
    If no sdcard or getExternalStorageState() is not MEDIA_MOUNTED or MEDIA_MOUNTED_READ_ONLY, the application will be crashed by storage path, and not use the PredefinedPath.

--- storagechooser/src/main/java/com/codekidlabs/storagechooser/fragments/ChooserDialogFragment.java
+++ storagechooser/src/main/java/com/codekidlabs/storagechooser/fragments/ChooserDialogFragment.java
@@ -232,7 +232,7 @@ public class ChooserDialogFragment extends android.app.DialogFragment {
         storagesList = new ArrayList<>();
 
         File storageDir = new File("/storage");
-        String internalStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String internalStoragePath =  mConfig.getPredefinedPath() != null ? mConfig.getPredefinedPath() : Environment.getExternalStorageDirectory().getAbsolutePath();
 
         File[] volumeList = storageDir.listFiles();
 
