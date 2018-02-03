1b37377000a4b95cce4bad13a04615fc444c4400
Elias Projahn <johrpan@gmail.com>
2018-02-03 20:57:43 +0100

    Optimize imports
    LinearLayout was an unused import here. Special thanks go to codacy for
    spotting this terrible bug!

--- storagechooser/src/main/java/com/codekidlabs/storagechooser/adapters/StorageChooserListAdapter.java
+++ storagechooser/src/main/java/com/codekidlabs/storagechooser/adapters/StorageChooserListAdapter.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
