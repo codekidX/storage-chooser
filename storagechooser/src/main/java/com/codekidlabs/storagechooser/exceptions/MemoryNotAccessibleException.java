5d086ac2aee7188ba0a89b051e9afbbb8c285ed0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-21 17:25:53 +0800

    Delete storagechooser directory

--- storagechooser/src/main/java/com/codekidlabs/storagechooser/exceptions/MemoryNotAccessibleException.java
+++
@@ -1,9 +0,0 @@
-package com.codekidlabs.storagechooser.exceptions;
-
-
-public class MemoryNotAccessibleException extends Exception {
-
-    public MemoryNotAccessibleException(String message) {
-        super(message);
-    }
-}
